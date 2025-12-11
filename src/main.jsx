e3b0d75f2872a8361262e03a1cd6311dc63f182d
Saagar Gahlot <saagargahlot@gmail.com>
2025-12-10 23:54:41 -0700

    changes overall again

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Portfolio from './portfolio.js'
+import Portfolio from './portfolio.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
